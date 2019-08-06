d33fa8ef97c482c8d6d75957f321aeead9ef78eb
Noah Jame Yanga <yanganoah404@gmail.com>
2019-08-06 13:17:54 +0800

    initial commit

---
+++ src/Utils.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author noahjamesy
+ */
+public class Utils {
+
+    /**
+     *
+     */
+    public static final String [][] data_types_equivalent = {
+        {"varchar","String"},
+        {"text","String"},
+        {"int","long"},
+        {"char","char"},
+        {"tinytext","String"},
+        {"text","String"},
+        {"mediumtext","String"},
+        {"longtext","String"},
+        {"tinyint","int"},
+        {"smallint","int"},
+        {"mediumint","int"},
+        {"int","int"},
+        {"bigint","long"},
+        {"float","float"},
+        {"double","double"},
+        {"decimal","double"},
+        {"date","String"},
+        {"datetime","String"},
+        {"timestamp","String"},
+        {"time","String"}, 
+    };
+}
